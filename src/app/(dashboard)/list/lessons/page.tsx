fc06cb7302277a78293bf77bca9f67f655aaff8c
hermantoXYZ <andiupdates@gmail.com>
2025-03-07 21:03:41 +0700

    push

---
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -0,0 +1,96 @@
+import Pagination from "@/components/Pagination"
+import TableSearch from "@/components/TableSearch"
+import Image from "next/image"
+import Table from "@/components/Table"
+import Link from "next/link"
+import { lessonsData, role } from "@/lib/data"
+
+
+type Lessons = {
+    id: number;
+    subject: string;
+    teacher: string;
+    class: string;
+
+  };
+    
+    const columns = [
+        {
+        header: "Subject Name",
+        accessor: "name",
+        },
+        {
+        header: "Teacher",
+        accessor: "teacher",
+        className: "hidden md:table-cell",
+        },
+        {
+        header: "Class",
+        accessor: "class",
+        },
+        {
+        header: "Actions",
+        accessor: "action",
+        },
+    ];
+
+const LessonListPage = () => {
+
+    const renderRow = (item:Lessons) => (
+        
+        <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-hermantoSkyLight">
+            <td className="flex items-center gap-4 p-4">{item.subject}</td>
+            <td>{item.class}</td>
+            <td className="hidden md:table-cell">{item.teacher}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                <Link href={`/list/teachers/${item.id}`}>
+                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-hermantoSky">
+                    <Image src="/edit.png" alt="" width={16} height={16} />
+                </button>
+                </Link>
+                {role === "admin" && (<button className="w-7 h-7 flex items-center justify-center rounded-full bg-hermantoPurpleLight">
+                    <Image src="/delete.png" alt="" width={16} height={16} />
+                </button>)}
+                </div>
+            </td>
+        </tr>
+
+    );
+
+
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            {/* TOP SECTION */}
+        <div className="flex items-center justify-between">
+            <h1 className="hidden md:block text-lg font-semibold">All Lessons</h1>
+            <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+            <TableSearch />
+            <div className="flex items-center gap-4 self-end">
+                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-hermantoYellow">
+                    <Image src="/filter.png" alt="" width={20} height={20} />
+                </button>
+                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-hermantoYellow">
+                    <Image src="/sort.png" alt="" width={20} height={20} />
+                </button>
+                {role ==="admin" && (<button className="w-8 h-8 flex items-center justify-center rounded-full bg-hermantoYellow">
+                    <Image src="/plus.png" alt="" width={20} height={20} />
+                </button>)}
+
+            </div>
+            </div>
+        </div>
+
+            {/* LIST SECTION */}
+        <div className="">
+        <Table columns={columns} renderRow={renderRow} data={lessonsData} />
+        </div>
+            {/* PAGE SECTION */}
+        <div className="">
+            <Pagination />
+        </div>
+        </div>
+    )
+}
+
+export default LessonListPage
